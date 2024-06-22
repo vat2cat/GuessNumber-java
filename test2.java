d18c110af960c6e9fa40e84634c3071a17eda466
Jean <160461838+vat2cat@users.noreply.github.com>
2024-06-22 23:15:29 +0800

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,66 +1,80 @@
 package homework002;
-import java.util.ArrayList;
-public class test2 
-{
-	public static void main(String[] a)
-	{
-		int n=0,x=1; String s=""; 
-		for(int c=1;c<=5;c++)
-		{
-			if(c==1)
-			{ s=func.Input("§A·Q¥ý²qÁÙ¬O«á²q¡A¥ý²q«ö0«á²q«ö1"); }
-			else
-			{ s=func.Input("½Ð¿ï¾Ü»P¥ý«e¤£¦Pªº¼Æ¦r"); }
-			n=Integer.parseInt(s);
-			if(n==0)
-			{
-				String ans=func.GuessAnswer(); //¤H²q¹q¸£
-				String gus="";AB p=new AB(); String z1; int cnt=1;
-				do
-				{
-					do
-					{
-						do
-						{
-							gus=func.Input("("+cnt+")¿é¤J¥|­Ó¤£¦Pªº¼Æ¦r¡G");
-					
-						}while(gus.length()!=4);
-					}while(func.CheckDifferent(gus)==false);
-					cnt++; p=func.CountAB2(ans, gus);
-					System.out.println(p.A+"A"+p.B+"B");
-				}while(p.A!=4);
-				System.out.println("ans¡G"+ans+"you guess"+(cnt-1)+"¦¸");
-			}
-			else //¹q¸£²q¤H
-			{
-				ArrayList<String> sol1=new ArrayList<String>();
-				ArrayList<String> sol2=new ArrayList<String>();
-				String gus=""; String z=""; int pt=0; int pt2=0; AB pp; int A=0,B=0;
-				for(int i=1023; i<9876; i++)
-				{
-					z=String.valueOf(i);
-					if(func.CheckDifferent(z)==true) {sol1.add(z);}
-				}
-				do
-				{
-					int k=func.RandMN(0, pt-1);
-					gus=sol1.get(k);
-					System.out.println("my guess..."+gus);
-					A=func.InputNum("¡HA "); B=func.InputNum("¡HB ");
-					pt2=0;
-					for(int i=0;i<pt;i++)
-					{
-						pp=func.CountAB2(gus, sol1.get(i));
-						if((pp.A==A)&&(pp.B==B)) {sol2.add(sol1.get(i));}
-					}
-					System.out.println(pt2);
-					sol1.clear();sol1.addAll(sol2);
-					pt=pt2;
-				}while(pt>1);
-				if(pt==1) {System.out.println("answer is ..."+sol1.get(0));}
-				else {System.out.println("you lose");}
-			}
-		}
-	}
-}
 
+import java.util.ArrayList;
+
+public class test2 {
+    public static void main(String[] a) {
+        int n = 0;
+        String s = "";
+        for (int c = 1; c <= 5; c++) {
+            if (c == 1) {
+                s = func.Input("ä½ æƒ³å…ˆçŒœé‚„æ˜¯å¾ŒçŒœï¼Œå…ˆçŒœæŒ‰0å¾ŒçŒœæŒ‰1");
+            } else {
+                s = func.Input("è«‹é¸æ“‡èˆ‡å…ˆå‰ä¸åŒçš„æ•¸å­—");
+            }
+            n = Integer.parseInt(s);
+            if (n == 0) {
+                // äººçŒœé›»è…¦
+                String ans = func.GuessAnswer();
+                String gus = "";
+                AB p = new AB();
+                int cnt = 1;
+                do {
+                    do {
+                        do {
+                            gus = func.Input("(" + cnt + ")è¼¸å…¥å››å€‹ä¸åŒçš„æ•¸å­—ï¼š");
+                        } while (gus.length() != 4);
+                    } while (!func.CheckDifferent(gus));
+                    cnt++;
+                    p = func.CountAB2(ans, gus);
+                    System.out.println(p.A + "A" + p.B + "B");
+                } while (p.A != 4);
+                System.out.println("ansï¼š" + ans + " you guess " + (cnt - 1) + " æ¬¡");
+            } else {
+                // é›»è…¦çŒœäºº
+                ArrayList<String> sol1 = new ArrayList<>();
+                ArrayList<String> sol2 = new ArrayList<>();
+                String gus = "";
+                String z = "";
+                int pt = 0;
+                int pt2 = 0;
+                AB pp;
+                int A = 0, B = 0;
+
+                for (int i = 1023; i <= 9876; i++) {
+                    z = String.valueOf(i);
+                    if (func.CheckDifferent(z)) {
+                        sol1.add(z);
+                    }
+                }
+                
+                pt = sol1.size();
+                
+                do {
+                    int k = func.RandMN(0, pt - 1);
+                    gus = sol1.get(k);
+                    System.out.println("my guess... " + gus);
+                    A = func.InputNum("ï¼ŸA ");
+                    B = func.InputNum("ï¼ŸB ");
+                    sol2.clear();
+                    pt2 = 0;
+                    for (int i = 0; i < pt; i++) {
+                        pp = func.CountAB2(gus, sol1.get(i));
+                        if (pp.A == A && pp.B == B) {
+                            sol2.add(sol1.get(i));
+                            pt2++;
+                        }
+                    }
+                    sol1.clear();
+                    sol1.addAll(sol2);
+                    pt = pt2;
+                } while (pt > 1);
+                if (pt == 1) {
+                    System.out.println("answer is ... " + sol1.get(0));
+                } else {
+                    System.out.println("you lose");
+                }
+            }
+        }
+    }
+}
